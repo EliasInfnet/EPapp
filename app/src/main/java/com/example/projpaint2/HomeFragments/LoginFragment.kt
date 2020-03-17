ea9b4e4c3158dbdc13e76374323162aebd1211e5
Elias <elias.carvalho@al.infnet.edu.br>
2020-03-17 02:55:35 -0300

    Teste elias

---
+++ app/src/main/java/com/example/projpaint2/HomeFragments/LoginFragment.kt
@@ -0,0 +1,36 @@
+package com.example.projpaint2.HomeFragments
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+
+import com.example.projpaint2.R
+import kotlinx.android.synthetic.main.fragment_login.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnCadastro.setOnClickListener {
+            findNavController().navigate(R.id.cadastroFragment)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/projpaint2/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.projpaint2
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
